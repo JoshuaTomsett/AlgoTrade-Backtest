d4e99bf2ec9e9657bbc0ab0dc6330e3501d4e6a2
Josh <josh.james.tomsett@gmail.com>
2023-10-12 16:11:34 +0100

    added adfuller test import (idk how it was removed)

--- Stockdata.py
+++ Stockdata.py
@@ -4,6 +4,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime , timedelta
+from statsmodels.tsa.stattools import adfuller
 
 yf.pdr_override()
 
@@ -15,3 +16,20 @@ def get_data_days(ticker, days, col='Adj Close'):
     stock_data = data.get_data_yahoo(ticker,start=WEEK_DATE,end=END_DATE)
     clean_data = stock_data[col]
     return clean_data.fillna(method='ffill') # returns only the data about prices
+
+
+def is_stationary(close_prices):
+  """Performs an ADF test on a list of close prices and returns True if it is stationary.
+
+  Args:
+    close_prices: A list of close prices for a stock.
+
+  Returns:
+    True if the stock prices are stationary, False otherwise.
+  """
+
+  # If the p-value < significance level, reject the null hypothesis and conclude that the series is not stationary.
+  if adfuller(close_prices)[1] < 0.05: return False
+
+  # Otherwise, we fail to reject the null hypothesis and conclude that the series is stationary.
+  else: return True
--- Strategies/MeanReversion.py
+++ Strategies/MeanReversion.py
@@ -2,8 +2,9 @@
 
 ####### Strategy Theory
 
-# Augmented Dicky-Fuller Test (ADF) - test for stationarity (if statistical properties do not vary with time)
-
+# Bollinger lines
+# Buy -> lower bound
+# Sell -> Upper bound
 
 
 import sys
@@ -12,6 +13,7 @@ sys.path.append("..")
 import numpy as np
 import matplotlib.pyplot as plt
 import Stockdata
+
 
 def calculate_moving_average(close_prices, window):
     """Calculates an array of moving averages for the given close prices in a given window
@@ -55,45 +57,29 @@ days = 2000 / 5 * 7
 
 ##### CALCULATIONS #####
 
+# create moving average lists
 close_prices = close_prices(ticker, days+200)
-# moving_averages_20 = calculate_moving_average(close_prices, 20)
+moving_averages_20 = calculate_moving_average(close_prices, 20)
 moving_averages_50 = calculate_moving_average(close_prices, 50)
 moving_averages_200 = calculate_moving_average(close_prices, 200)
+
 # formatting for plot
 close_prices = close_prices[199:]
-# moving_averages_20 = moving_averages_20[180:]
+moving_averages_20 = moving_averages_20[180:]
 moving_averages_50 = moving_averages_50[150:]
-
-
-##### TEST FOR STATIONARITY #####
-
-def is_stationary(close_prices):
-  """Performs an ADF test on a list of close prices and returns True if it is stationary.
-
-  Args:
-    close_prices: A list of close prices for a stock.
-
-  Returns:
-    True if the stock prices are stationary, False otherwise.
-  """
-
-  # If the p-value < significance level, reject the null hypothesis and conclude that the series is not stationary.
-  if adfuller(close_prices)[1] < 0.05: return False
-
-  # Otherwise, we fail to reject the null hypothesis and conclude that the series is stationary.
-  else: return True
 
 
 ##### PLOTTING #####
 
-plt.figure(figsize=(10, 6))
-plt.plot(close_prices, label='Closing Prices')
+# plt.figure(figsize=(10, 6))
+# plt.plot(close_prices, label='Closing Prices')
 # plt.plot(moving_averages_20, label='SMA20')
-plt.plot(moving_averages_50, label='SMA50')
-plt.plot(moving_averages_200, label='SMA200')
-plt.xlabel('Period')
-plt.ylabel('Price')
-plt.title(f'Closing Prices and Moving Averages - {ticker}')
-plt.legend()
-plt.grid(True)
-plt.show()
+# plt.plot(moving_averages_50, label='SMA50')
+# plt.plot(moving_averages_200, label='SMA200')
+# plt.xlabel('Period')
+# plt.ylabel('Price')
+# plt.title(f'Closing Prices and Moving Averages - {ticker}')
+# plt.legend()
+# plt.grid(True)
+# plt.show()
+
